2b54021a0b534f54295845bc3cc037df18228f08
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2024-03-22 21:37:27 -0400

    Create Application.kt

---
+++ src/main/kotlin/com/e/Application.kt
@@ -0,0 +1,12 @@
+package com.e
+
+import com.e.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
